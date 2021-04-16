6d88d868de678ee5e9bfce0d66db98e2fa132e6c
danielapochini <daniela.pochini@fatec.sp.gov.br>
2021-04-16 09:44:41 -0300

    Criação de Testes Exception e Regex

---
+++ DesafioAutomacaoAPI/Model/Response/ErrorMessageResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioAutomacaoAPI.Model
+{
+    public class ErrorMessageResponse
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("localized")]
+        public string Localized { get; set; }
+    }
+}
