8c569381c2ef6f5a93b9746164c2ebf2f00b4c5d
danielapochini <daniela.pochini@fatec.sp.gov.br>
2021-04-17 17:29:30 -0300

    Correção nome do ambiente conforme meta 8

--- DesafioAutomacaoAPI/Utils/Settings/LaunchSettingsFixture.cs
+++ DesafioAutomacaoAPI/Utils/Settings/LaunchSettingsFixture.cs
@@ -27,11 +27,11 @@ namespace DesafioAutomacaoAPI.Utils.Settings
                                    .SelectMany(profile => profile.Children<JProperty>())
                                    .ToList();
 
-#if QA
-                var variaveisQa = variables.Where(prop => prop.Path == "profiles.DesafioAutomacaoAPI:QA.environmentVariables")
+#if DEV
+                var variaveisDev = variables.Where(prop => prop.Path == "profiles.DesafioAutomacaoAPI:DEV.environmentVariables")
                                     .SelectMany(prop => prop.Value.Children<JProperty>())
                                     .ToList();
-                foreach (var variable in variaveisQa)
+                foreach (var variable in variaveisDev)
                     Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
 #endif
 #if HML
